a2e9a7948b079d12874ed594cb62c6d1416b9a89
Ali Ansari <47219029+Allliance@users.noreply.github.com>
2024-04-24 22:41:41 +0330

    Update Load_Model.py

--- Odysseus/Models/Load_Model.py
+++ Odysseus/Models/Load_Model.py
@@ -11,19 +11,21 @@ import pandas as pd
 import os
 from os import path
 import math
-from Cifar10_models import *
-from mnist_architectures import Model_Google_1,Model_Google_2,Model_Google_3,Model_Google_4
+from .Cifar10_models import *
+from .mnist_architectures import Model_Google_1,Model_Google_2,Model_Google_3,Model_Google_4
 
 
 # model loader for mnist models
-def load_mnist_model(model_path, device,num_class=10):
-    checkpoint = torch.load(model_path)
-    print("keys are :", checkpoint.keys())
+def load_mnist_model(model_path, device,num_class=10, log=False):
+    checkpoint = torch.load(model_path, map_location=device)
+    if log:
+        print("keys are :", checkpoint.keys())
 
     model = checkpoint['Architecture_Name']
 
     # Get the model
-    print('==> Building model..')
+    if log:
+        print('==> Building model..')
     if model == 'Model_Google_1':
         net = Model_Google_1()
     elif model == 'Model_Google_2':
@@ -38,13 +40,16 @@ def load_mnist_model(model_path, device,num_class=10):
 
     if 'test_clean_acc' in checkpoint:
         best_acc_clean = checkpoint['test_clean_acc']
-        print("The Accuracies on clean samples:  ", best_acc_clean)
+        if log:
+            print("The Accuracies on clean samples:  ", best_acc_clean)
     if 'test_trigerred_acc' in checkpoint:
         best_acc_trig = checkpoint['test_trigerred_acc']
-        print("The fooling rate: ", best_acc_trig)
+        if log:
+            print("The fooling rate: ", best_acc_trig)
     if 'Mapping' in checkpoint:
         mapping = checkpoint['Mapping']
-        print("Mapping is : ",mapping, type(mapping))
+        if log:
+            print("Mapping is : ",mapping, type(mapping))
         if isinstance(mapping,int) or isinstance(mapping,np.float64):
             mapping=mapping*np.ones(num_class,dtype=float)
         elif isinstance(mapping,str):
